a393400ce363f63e954c7a2cbb9401c58dbb795a
Alesia <andomyel@yandex.ru>
2022-11-13 23:29:09 +0300

    task66

--- HW9C#/Program.cs
+++ HW9C#/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+/*
 Console.WriteLine("Введите целое число N больше 0:");
 int N = int.Parse(Console.ReadLine()!);
 if (N < 1) Console.WriteLine("Вы ввели число 0 или меньше");
@@ -8,4 +10,25 @@ else Console.WriteLine(PrintNumbersNto1 (N, 1));
 string PrintNumbersNto1 (int start, int end){
     if (start == 1) return start.ToString();
     return (start + " " + PrintNumbersNto1 (start - 1, end));
+}
+*/
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Введите целое число M:");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите целое число N:");
+int N = int.Parse(Console.ReadLine()!);
+
+if (M <= N){
+Console.WriteLine(SumNumbersMtoN (M, N));
+}
+else {
+Console.WriteLine(SumNumbersMtoN (N, M));
+}
+
+
+
+int SumNumbersMtoN (int start, int end){
+    if (start == end) return start;
+        return  start + SumNumbersMtoN (start + 1, end);
 }
